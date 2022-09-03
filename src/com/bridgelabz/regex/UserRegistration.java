654ae5958d7107445fac64b08f42f878def2041a
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-09-03 23:19:01 +0530

    Modified to check for valid phone number

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -46,11 +46,25 @@ public class UserRegistration {
         }
     }
 
+    public void validPhoneNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Phone number: ");
+        String phoneNumber = sc.nextLine();
+        boolean number = Pattern.matches("^[9][1][ ][6-9][0-9]{9}$", phoneNumber);
+
+        if (number) {
+            System.out.println("Phone number is correct");
+        } else {
+            System.out.println("Phone number is incorrect");
+        }
+    }
+
     public static void main(String[] args) {
         UserRegistration userRegistration = new UserRegistration();
 //        userRegistration.validFirstName();
 //        userRegistration.validLastName();
-        userRegistration.validEmailId();
+//        userRegistration.validEmailId();
+        userRegistration.validPhoneNumber();
     }
 
 }
